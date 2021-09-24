7c57e801dd47c7edd155d22fa43e1b3115739832
LenNguyenVan13 <nguyenvanlentech@gmail.com>
2021-09-24 14:36:46 +0700

    Chuyển đổi giao diện 1 - 2 OK, xử lý xong share implement

--- app/src/main/java/com/se/animation_share_element_transition/MainActivity.java
+++ app/src/main/java/com/se/animation_share_element_transition/MainActivity.java
@@ -2,7 +2,9 @@ package com.se.animation_share_element_transition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       findViewById(R.id.btn_go).setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               startActivity(new Intent( MainActivity.this,
+                       list_flower.class));
+
+               overridePendingTransition(R.anim.enter_x, R.anim.exit_x);
+           }
+
+       });
     }
 }
---
+++ app/src/main/java/com/se/animation_share_element_transition/share_element_tree.java
@@ -0,0 +1,14 @@
+package com.se.animation_share_element_transition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class share_element_tree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_element_tree);
+    }
+}
