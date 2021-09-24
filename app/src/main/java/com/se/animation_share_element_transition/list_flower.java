88680df780956fb96857a4e41c52cdb7dcaa6181
LenNguyenVan13 <nguyenvanlentech@gmail.com>
2021-09-24 13:34:59 +0700

    Animation basic

---
+++ app/src/main/java/com/se/animation_share_element_transition/list_flower.java
@@ -0,0 +1,14 @@
+package com.se.animation_share_element_transition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class list_flower extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_flower);
+    }
+}
